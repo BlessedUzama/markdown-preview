cdac33d0fa2b084c99793719a3e06dd6e7ce0d1f
BlessedUzama <uzamablessed@gmail.com>
2025-10-01 10:10:19 +0100

    Initail commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <main className="h-screen flex items-center justify-center bg-gray-100">
+      <button className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700">
+        Tailwind is working 🎉
+      </button>
+    </main>
+  );
+}
+
+export default App;
+
